6aa5690d1187e54c660da8956974950ca97c455a
AUST1E <102739978+AUST1E@users.noreply.github.com>
2022-03-31 23:33:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
         Literal = []
         Literal[False]= true
         uploaded_file = st.sidebar.file_uploader(
-            "上传图片", Literal= true,type=['png', 'jpeg', 'jpg'])
+            "上传图片",type=['png', 'jpeg', 'jpg']，bool = true)
         if uploaded_file is not None:
             is_valid = True
             with st.spinner(text='资源加载中...'):
