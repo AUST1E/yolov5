fd83e8c2f0cef78c4b47778b9e0e571fa66e8cce
AUST1E <102739978+AUST1E@users.noreply.github.com>
2022-04-05 23:19:26 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -218,7 +218,7 @@ def detect(opt,save_img=False):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/yolov5s.pt',
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt',
                         help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='data/images',
                         help='source')  # file/folder, 0 for webcam
