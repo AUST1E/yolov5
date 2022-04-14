e7b1c5629cf9080a7c05b06d9d4c478fd45df034
AUST1E <102739978+AUST1E@users.noreply.github.com>
2022-04-15 00:12:38 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -9,7 +9,6 @@ import pymssql
 import datetime
 import matplotlib
 import os
-matplotlib.use('TKAgg')
 from matplotlib import pyplot as plt
 from numpy import random
 
@@ -19,7 +18,7 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
-
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" #允许副本存在，忽略报错
 
 def detect(opt,connect,save_img=False):
     global cls
