95b3a01bfe3ef58da8d8dd7a7b31d1f9f4350a8f
AUST1E <102739978+AUST1E@users.noreply.github.com>
2022-04-16 19:25:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -577,7 +577,7 @@ def searchByerro():
 def run_detect():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str,
-                        default='weights/yolov5s.pt', help='model.pt path(s)')
+                        default='yolov5s.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str,
                         default='data/images', help='source')
     parser.add_argument('--img-size', type=int, default=640,
